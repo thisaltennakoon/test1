0ff9a6a0c5f60ba84251d34068aff48372fd4d31
Thisal Tennakoon <thisaltennakoon@gmail.com>
2021-01-17 12:53:06 +0530

    a

---
+++ created_npm_library_test/index.js
@@ -0,0 +1,6 @@
+const webpackNumbers = require('webpack-numbers');
+// ...
+// webpackNumbers.wordToNum('Two');
+
+console.log(webpackNumbers.webpackNumbers.wordToNum('Two'));
+console.log(webpackNumbers.webpackNumbers.numToWord(2));
